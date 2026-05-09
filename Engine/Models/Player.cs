70ecf9960b03ce48fbbbcacb6f5f3e210f13fcfc
Milo van Pelt <milovanpelt@gmail.com>
2026-05-09 12:56:41 +0200

    chore: move WPFUI to its own folder + create Engine project to store data and models

---
+++ Engine/Models/Player.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Engine.Models
+{
+    internal class Player
+    {
+        public  string? Name { get; set; }
+        public string? CharacterClass { get; set; }
+        public int HitPoints { get; set; }
+        public int ExperincePoints { get; set; }
+        public int Level { get; set; }
+        public int Gold { get; set; }
+    }
+}
---
+++ Engine/ViewModels/GameSession.cs
@@ -0,0 +1,18 @@
+﻿using Engine.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Engine.ViewModels
+{
+    internal class GameSession
+    {
+        private Player? CurrentPlayer { get; set; }
+
+        public GameSession() 
+        { 
+            CurrentPlayer = new Player();
+            CurrentPlayer.Name = string.Empty;
+        }
+    }
+}
